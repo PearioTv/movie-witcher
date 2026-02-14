0c8cd5a36f5324544282a71d471b5da5d2ef801f
Peario <plumethief@gmail.com>
2026-02-14 02:07:36 +0100

    Update Stream.vue

--- src/views/Stream/Stream.vue
+++ src/views/Stream/Stream.vue
@@ -4,7 +4,7 @@
       <div class="blur"></div>
       <div
         class="image"
-        :style="`background-image: url(${meta.background})`"
+        :style="background-image: url(${meta.background})"
         v-if="meta && meta.background"
       ></div>
     </div>
@@ -12,8 +12,16 @@
     <div class="content-container">
       <div class="meta" v-if="meta">
         <div class="meta-center-wrapper">
-          <img class="logo" :src="meta.logo" alt="" v-if="meta.logo" />
-          <div class="title" v-else>{{ meta.name }}</div>
+          <img
+            class="logo"
+            :src="meta.logo"
+            alt=""
+            v-if="meta.logo"
+          />
+
+          <div class="title" v-else>
+            {{ meta.name }}
+          </div>
 
           <div class="details">
             <div class="year">{{ meta.year }}</div>
@@ -23,10 +31,16 @@
             </div>
           </div>
 
-          <div class="description">{{ meta.description }}</div>
+          <div class="description">
+            {{ meta.description }}
+          </div>
 
           <div class="tags">
-            <div class="tag" v-for="genre in meta.genres" :key="genre">
+            <div
+              class="tag"
+              v-for="genre in meta.genres"
+              :key="genre"
+            >
               {{ genre }}
             </div>
           </div>
@@ -67,9 +81,12 @@
       <div class="series-navigation" v-if="isSeries">
         <div class="section-header">
           <h3>{{ t('views.stream.season') }}</h3>
+
           <Segments :segments="seasons" v-model="selectedSeason">
             <template #segment="{ segment }">
-              <span>{{ t('views.stream.season') }} {{ segment }}</span>
+              <span>
+                {{ t(views.stream.season) }} {{ segment }}
+              </span>
             </template>
           </Segments>
         </div>
@@ -84,15 +101,18 @@
           >
             <div
               class="ep-thumbnail"
-              :style="ep.thumbnail ? `background-image: url(${ep.thumbnail})` : ''"
+              :style="ep.thumbnail ? background-image: url(${ep.thumbnail}) : ''"
             >
-              <div class="ep-number">{{ ep.episode }}</div>
+              <div class="ep-number">
+                {{ ep.episode }}
+              </div>
             </div>
 
             <div class="ep-info">
               <div class="ep-name">
-                {{ ep.name || `${t('views.stream.episode')} ${ep.episode}` }}
+                {{ ep.name || ${t('views.stream.episode')} ${ep.episode} }}
               </div>
+
               <div class="ep-aired" v-if="ep.released">
                 {{ new Date(ep.released).toLocaleDateString() }}
               </div>
@@ -105,13 +125,13 @@
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from "vue";
-import { useI18n } from "vue-i18n";
-import router from "@/router";
+import { computed, onMounted, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import router from '@/router';
 import StremioService from "@/services/stremio.service";
-import Button from "@/components/ui/Button.vue";
-import Segments from "@/components/ui/Segments.vue";
-import VidfastPlayer from "@/components/player/VidfastPlayer.vue";
+import Button from '@/components/ui/Button.vue';
+import Segments from '@/components/ui/Segments.vue';
+import VidfastPlayer from '@/components/player/VidfastPlayer.vue';
 
 const { t } = useI18n();
 
@@ -121,25 +141,30 @@ const selectedSeason = ref(1);
 const selectedEpisode = ref(null);
 const showPlayer = ref(false);
 
-const isSeries = computed(() => meta.value && meta.value.type === "series");
+const isSeries = computed(() =>
+  meta.value && meta.value.type === 'series'
+);
 
 const episodes = computed(() => {
   if (!meta.value || !meta.value.videos) return [];
+
   return meta.value.videos
     .filter((video) => video.season === selectedSeason.value)
     .sort((a, b) => a.episode - b.episode);
 });
 
-const selectedEpisodeNumber = computed(() =>
-  selectedEpisode.value ? selectedEpisode.value.episode : 1
-);
+const selectedEpisodeNumber = computed(() => {
+  return selectedEpisode.value
+    ? selectedEpisode.value.episode
+    : 1;
+});
 
 const openTrailer = () => {
   if (meta.value.trailers && meta.value.trailers.length) {
     const trailer = meta.value.trailers[0];
     window.open(
-      `https://www.youtube.com/watch?v=${trailer.source}`,
-      "_blank"
+      https://www.youtube.com/watch?v=${trailer.source},
+      '_blank'
     );
   }
 };
@@ -147,8 +172,12 @@ const openTrailer = () => {
 const selectEpisode = (ep) => {
   selectedEpisode.value = ep;
   showPlayer.value = true;
+
   router.replace({
-    params: { ...router.currentRoute.value.params, id: ep.id },
+    params: {
+      ...router.currentRoute.value.params,
+      id: ep.id
+    }
   });
 };
 
@@ -156,25 +185,81 @@ onMounted(async () => {
   const { type, id } = router.currentRoute.value.params;
 
   if (id && type) {
-    const [metaId] = id.split(":");
+    const [metaId] = id.split(':');
 
     meta.value =
-      type === "movie"
+      type === 'movie'
         ? await StremioService.getMetaMovie(metaId)
         : await StremioService.getMetaSeries(metaId);
 
-    if (meta.value && meta.value.videos && meta.value.videos.length) {
+    if (
+      meta.value &&
+      meta.value.videos &&
+      meta.value.videos.length
+    ) {
       const episode =
-        meta.value.videos.find(({ id: imdb_id }) => imdb_id === id) ||
-        meta.value.videos[0];
+        meta.value.videos.find(
+          ({ id: imdb_id }) => imdb_id === id
+        ) || meta.value.videos[0];
 
       selectedSeason.value = episode.season || 1;
       selectedEpisode.value = episode;
 
-      seasons.value = [...new Set(meta.value.videos.map(({ season }) => season))]
-        .filter((s) => s > 0)
+      seasons.value = [
+        ...new Set(
+          meta.value.videos.map(
+            ({ season }) => season
+          )
+        )
+      ]
+        .filter(s => s > 0)
         .sort((a, b) => a - b);
     }
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.stream {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 40px 5%;
+
+  .background {
+    z-index: -1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    .blur,
+    .image {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+    }
+
+    .blur {
+      z-index: 1;
+      backdrop-filter: blur(60px);
+      background-color: rgba(0, 0, 0, 0.85);
+    }
+
+    .image {
+      background-size: cover;
+      background-position: center;
+    }
+  }
+
+  .content-container {
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+  }
+}
+</style>
