86cdee1961141d07b6887b9502eaf35947622492
Peario <plumethief@gmail.com>
2026-02-14 05:33:01 +0100

    Update Stream.vue

--- src/views/Stream/Stream.vue
+++ src/views/Stream/Stream.vue
@@ -2,18 +2,24 @@
   <div class="stream">
     <div class="background">
       <div class="blur"></div>
-      <div class="image" :style="`background-image: url(${meta.background})`" v-if="meta && meta.background"></div>
+      <div
+        class="image"
+        :style="`background-image: url(${meta.background})`"
+        v-if="meta && meta.background"
+      ></div>
     </div>
 
     <div class="content-container">
       <div class="meta" v-if="meta">
-        <img class="logo" :src="meta.logo" alt="" v-if="meta.logo">
+        <img class="logo" :src="meta.logo" alt="" v-if="meta.logo" />
         <div class="title" v-else>{{ meta.name }}</div>
 
         <div class="details">
           <div class="year">{{ meta.year }}</div>
           <div class="runtime">{{ meta.runtime }}</div>
-          <div class="rating" v-if="meta.imdbRating">⭐ {{ meta.imdbRating }}</div>
+          <div class="rating" v-if="meta.imdbRating">
+            ⭐ {{ meta.imdbRating }}
+          </div>
         </div>
 
         <div class="description">{{ meta.description }}</div>
@@ -28,7 +34,13 @@
           <Button @click="openPlayer" icon="play-circle-outline">
             {{ t('views.stream.watch') }}
           </Button>
-          <Button v-if="meta.trailers && meta.trailers.length" type="secondary" @click="openTrailer" icon="videocam-outline">
+
+          <Button
+            v-if="meta.trailers && meta.trailers.length"
+            type="secondary"
+            @click="openTrailer"
+            icon="videocam-outline"
+          >
             {{ t('views.stream.trailer') }}
           </Button>
         </div>
@@ -37,15 +49,19 @@
 
     <!-- 🎬 POPUP PLAYER -->
     <transition name="popup">
-      <div v-if="showPlayer" class="player-overlay" @click.self="closePlayer">
+      <div
+        v-if="showPlayer"
+        class="player-overlay"
+        @click.self="closePlayer"
+      >
         <div class="player-modal">
           <span class="close-btn" @click="closePlayer">✕</span>
 
-          <VidfastPlayer 
-            :type="meta.type" 
-            :id="meta.imdb_id" 
-            :season="selectedSeason" 
-            :episode="selectedEpisodeNumber"
+          <VidfastPlayer
+            :type="meta.type"
+            :id="meta.imdb_id"
+            :season="1"
+            :episode="1"
           />
         </div>
       </div>
@@ -54,25 +70,17 @@
 </template>
 
 <script setup>
-import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
-import { useI18n } from 'vue-i18n';
-import router from '@/router';
+import { ref, onMounted, onUnmounted } from "vue";
+import { useI18n } from "vue-i18n";
+import router from "@/router";
 import StremioService from "@/services/stremio.service";
-import Button from '@/components/ui/Button.vue';
-import VidfastPlayer from '@/components/player/VidfastPlayer.vue';
+import Button from "@/components/ui/Button.vue";
+import VidfastPlayer from "@/components/player/VidfastPlayer.vue";
 
 const { t } = useI18n();
 
 const meta = ref({});
-const selectedSeason = ref(1);
-const selectedEpisode = ref(null);
 const showPlayer = ref(false);
-
-const isSeries = computed(() => meta.value && meta.value.type === 'series');
-
-const selectedEpisodeNumber = computed(() => {
-  return selectedEpisode.value ? selectedEpisode.value.episode : 1;
-});
 
 const openPlayer = () => {
   showPlayer.value = true;
@@ -85,28 +93,35 @@ const closePlayer = () => {
 };
 
 const handleEsc = (e) => {
-  if (e.key === 'Escape') closePlayer();
+  if (e.key === "Escape") closePlayer();
+};
+
+const openTrailer = () => {
+  if (meta.value.trailers && meta.value.trailers.length) {
+    const trailer = meta.value.trailers[0];
+    window.open(
+      `https://www.youtube.com/watch?v=${trailer.source}`,
+      "_blank"
+    );
+  }
 };
 
 onMounted(async () => {
-  window.addEventListener('keydown', handleEsc);
+  window.addEventListener("keydown", handleEsc);
 
   const { type, id } = router.currentRoute.value.params;
 
   if (id && type) {
-    const [metaId] = id.split(':');
-    meta.value = type === 'movie'
-      ? await StremioService.getMetaMovie(metaId)
-      : await StremioService.getMetaSeries(metaId);
-
-    if (meta.value?.videos?.length) {
-      selectedEpisode.value = meta.value.videos[0];
-    }
+    const [metaId] = id.split(":");
+    meta.value =
+      type === "movie"
+        ? await StremioService.getMetaMovie(metaId)
+        : await StremioService.getMetaSeries(metaId);
   }
 });
 
 onUnmounted(() => {
-  window.removeEventListener('keydown', handleEsc);
+  window.removeEventListener("keydown", handleEsc);
 });
 </script>
 
