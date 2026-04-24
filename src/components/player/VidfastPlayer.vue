f452cbe9c81a1d374ea238bfaad74e6a8c2f741e
Peario <plumethief@gmail.com>
2026-04-24 08:24:34 +0100

    Update VidfastPlayer.vue

--- src/components/player/VidfastPlayer.vue
+++ src/components/player/VidfastPlayer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="vidfast-player-container">
-    <div class="vidfast-player-aspect-ratio">
+  <div class="vidfast-wrapper">
+    <div class="vidfast-scaler">
       <iframe
         v-if="embedUrl"
         :src="embedUrl"
@@ -9,16 +9,13 @@
         scrolling="no"
         allow="autoplay; fullscreen; chromecast; encrypted-media"
       ></iframe>
-      <div v-else class="no-source">
-        No source available
-      </div>
+      <div v-else class="no-source">No source available</div>
     </div>
   </div>
 </template>
 
 <script setup>
 import { computed } from 'vue';
-
 const props = defineProps({
   type: { type: String, required: true },
   id: { type: String, required: true },
@@ -31,63 +28,57 @@ const embedUrl = computed(() => {
   const url = props.type === 'movie' 
     ? `https://vidfast.pro/movie/${props.id}` 
     : `https://vidfast.pro/tv/${props.id}/${props.season}/${props.episode}`;
-  // إضافة بارامترات لضمان عمل المشغل بشكل صحيح
   return `${url}?autoPlay=true&nextButton=true`;
 } );
 </script>
 
 <style lang="scss" scoped>
-.vidfast-player-container {
-  width: 100%;
-  max-width: 100%;
-  background: #000;
-  margin: 0 auto;
-}
-
-.vidfast-player-aspect-ratio {
+.vidfast-wrapper {
   position: relative;
   width: 100%;
-  // هذه النسبة (56.25%) هي التي تضمن ظهور شريط التحكم والترجمة
-  padding-bottom: 56.25%; 
-  height: 0;
-  overflow: hidden;
   background: #000;
+  overflow: hidden;
+  aspect-ratio: 16 / 9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
+.vidfast-scaler {
+  position: absolute;
+  // إجبار المشغل على رؤية مساحة عرض 1280px على الأقل لإظهار التحكم
+  width: 1280px; 
+  height: 720px;
+  
   iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
     width: 100%;
     height: 100%;
     border: none;
   }
-
-  .no-source {
-    position: absolute;
-    top: 0; left: 0; width: 100%; height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #fff;
-    font-family: 'Montserrat-Bold';
-  }
 }
 
-// تحسينات خاصة للهواتف
+// للهواتف: نقوم بتصغير المشغل ليتناسب مع الشاشة مع الحفاظ على أدوات التحكم
 @media (max-width: 768px) {
-  .vidfast-player-container {
-    // التأكد من أن المشغل يأخذ العرض الكامل للهاتف
-    width: 100vw;
-    margin-left: calc(-50vw + 50%);
+  .vidfast-wrapper {
+    height: auto;
+    aspect-ratio: 16 / 9;
+  }
+  
+  .vidfast-scaler {
+    // تصغير المشغل بصرياً ليتناسب مع عرض الهاتف
+    transform: scale(calc(100vw / 1280));
+    transform-origin: center center;
   }
 }
 
-// عند تدوير الهاتف للوضع الأفقي (Landscape)
+// عند تدوير الهاتف (الوضع الأفقي)
 @media (orientation: landscape) {
-  .vidfast-player-aspect-ratio {
-    // في الوضع الأفقي، نفضل أن يملأ المشغل الارتفاع أيضاً
-    padding-bottom: 0;
+  .vidfast-wrapper {
     height: 100vh;
+    aspect-ratio: auto;
+  }
+  .vidfast-scaler {
+    transform: scale(calc(100vh / 720));
   }
 }
 </style>
