e62166fd5fccf59feb6bf27378aa81efbfa4f014
Peario <plumethief@gmail.com>
2026-04-28 00:38:23 +0100

    Update VidfastPlayer.vue

--- src/components/player/VidfastPlayer.vue
+++ src/components/player/VidfastPlayer.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="vidfast-player-wrapper">
-    <iframe
-      v-if="embedUrl"
-      :src="embedUrl"
-      frameborder="0"
-      allowfullscreen
-      scrolling="no"
-      allow="autoplay; fullscreen; chromecast; encrypted-media"
-      class="vidfast-iframe"
-    ></iframe>
-    <div v-else class="no-source">No source available</div>
+  <div class="vidfast-outer-wrapper">
+    <div class="vidfast-scaler">
+      <iframe
+        v-if="embedUrl"
+        :src="embedUrl"
+        frameborder="0"
+        allowfullscreen
+        scrolling="no"
+        allow="autoplay; fullscreen; chromecast; encrypted-media"
+      ></iframe>
+      <div v-else class="no-source">No source available</div>
+    </div>
   </div>
 </template>
 
@@ -27,48 +28,68 @@ const embedUrl = computed(() => {
   const url = props.type === 'movie' 
     ? `https://vidfast.pro/movie/${props.id}` 
     : `https://vidfast.pro/tv/${props.id}/${props.season}/${props.episode}`;
-  // إضافة البارامترات التي تجبر المشغل على إظهار الواجهة الكاملة
-  return `${url}?autoPlay=true`;
-} );
+  return `${url}?autoPlay=true&nextButton=true`;
+});
 </script>
 
 <style lang="scss" scoped>
-.vidfast-player-wrapper {
+.vidfast-outer-wrapper {
   position: relative;
   width: 100%;
-  /* استخدام نسبة 16:9 بدقة متناهية لضمان ظهور شريط الأدوات السفلي */
-  aspect-ratio: 16 / 9;
   background: #000;
   overflow: hidden;
+  aspect-ratio: 16 / 9; 
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.vidfast-iframe {
+.vidfast-scaler {
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border: none;
-  /* ضمان أن اللمس يعمل على كافة أجزاء المشغل */
-  pointer-events: auto;
-}
+  width: 1280px; 
+  height: 720px;
+  transform-origin: center center;
+  
+  /* للهواتف: نقوم بتصغير المشغل ليتناسب مع عرض الشاشة */
+  transform: scale(calc(100vw / 1280));
 
-/* تحسينات خاصة للهواتف لضمان عدم اختفاء الأزرار */
-@media (max-width: 768px) {
-  .vidfast-player-wrapper {
-    /* زيادة الارتفاع قليلاً في الهواتف ليعطي مساحة للأزرار السفلية */
-    aspect-ratio: 16 / 10; 
-    width: 100vw;
-    margin-left: calc(-50vw + 50%);
+  iframe {
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+
+  .no-source {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    color: #fff;
+    font-family: 'Montserrat-Bold';
   }
 }
 
-/* الوضع الأفقي (Landscape) وهو الأهم للمشاهدة */
+@media (min-width: 1280px) {
+  .vidfast-scaler {
+    transform: scale(1);
+    width: 100%;
+    height: 100%;
+  }
+}
+
+@media (max-width: 1279px) and (min-width: 769px) {
+  .vidfast-scaler {
+    transform: scale(calc(90vw / 1280));
+  }
+}
+
 @media (orientation: landscape) {
-  .vidfast-player-wrapper {
+  .vidfast-outer-wrapper {
     height: 100vh;
-    width: 100vw;
     aspect-ratio: auto;
+  }
+  .vidfast-scaler {
+    transform: scale(calc(100vh / 720));
   }
 }
 </style>
