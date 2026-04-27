ffe75c2a3b9e0133cda7258ca92e10662667786b
Peario <plumethief@gmail.com>
2026-04-28 00:33:40 +0100

    Update VidfastPlayer.vue

--- src/components/player/VidfastPlayer.vue
+++ src/components/player/VidfastPlayer.vue
@@ -1,24 +1,20 @@
 <template>
-  <div class="vidfast-player-container">
-    <div class="vidfast-player-aspect-ratio">
-      <iframe
-        v-if="embedUrl"
-        :src="embedUrl"
-        frameborder="0"
-        allowfullscreen
-        scrolling="no"
-        allow="autoplay; fullscreen; chromecast; encrypted-media"
-      ></iframe>
-      <div v-else class="no-source">
-        No source available
-      </div>
-    </div>
+  <div class="vidfast-player-wrapper">
+    <iframe
+      v-if="embedUrl"
+      :src="embedUrl"
+      frameborder="0"
+      allowfullscreen
+      scrolling="no"
+      allow="autoplay; fullscreen; chromecast; encrypted-media"
+      class="vidfast-iframe"
+    ></iframe>
+    <div v-else class="no-source">No source available</div>
   </div>
 </template>
 
 <script setup>
 import { computed } from 'vue';
-
 const props = defineProps({
   type: { type: String, required: true },
   id: { type: String, required: true },
@@ -31,61 +27,48 @@ const embedUrl = computed(() => {
   const url = props.type === 'movie' 
     ? `https://vidfast.pro/movie/${props.id}` 
     : `https://vidfast.pro/tv/${props.id}/${props.season}/${props.episode}`;
-  return `${url}?autoPlay=true&nextButton=true`;
-});
+  // إضافة البارامترات التي تجبر المشغل على إظهار الواجهة الكاملة
+  return `${url}?autoPlay=true`;
+} );
 </script>
 
 <style lang="scss" scoped>
-.vidfast-player-container {
-  width: 100%;
-  max-width: 100%;
-  background: #000;
-  margin: 0 auto;
-  border-radius: 12px;
-  overflow: hidden;
-  box-shadow: 0 10px 30px rgba(0,0,0,0.5);
-}
-
-.vidfast-player-aspect-ratio {
+.vidfast-player-wrapper {
   position: relative;
   width: 100%;
-  padding-bottom: 56.25%; /* 16:9 Aspect Ratio */
-  height: 0;
-  overflow: hidden;
+  /* استخدام نسبة 16:9 بدقة متناهية لضمان ظهور شريط الأدوات السفلي */
+  aspect-ratio: 16 / 9;
   background: #000;
-
-  iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border: none;
-  }
-
-  .no-source {
-    position: absolute;
-    top: 0; left: 0; width: 100%; height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #fff;
-    font-family: 'Montserrat-Bold';
-  }
+  overflow: hidden;
 }
 
+.vidfast-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  /* ضمان أن اللمس يعمل على كافة أجزاء المشغل */
+  pointer-events: auto;
+}
+
+/* تحسينات خاصة للهواتف لضمان عدم اختفاء الأزرار */
 @media (max-width: 768px) {
-  .vidfast-player-container {
-    border-radius: 0;
+  .vidfast-player-wrapper {
+    /* زيادة الارتفاع قليلاً في الهواتف ليعطي مساحة للأزرار السفلية */
+    aspect-ratio: 16 / 10; 
     width: 100vw;
     margin-left: calc(-50vw + 50%);
   }
 }
 
+/* الوضع الأفقي (Landscape) وهو الأهم للمشاهدة */
 @media (orientation: landscape) {
-  .vidfast-player-aspect-ratio {
-    padding-bottom: 0;
+  .vidfast-player-wrapper {
     height: 100vh;
+    width: 100vw;
+    aspect-ratio: auto;
   }
 }
 </style>
